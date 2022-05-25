389241802059b3fcd94a90fba5e74df05f3d2f47
Crystal <crystal120599@gmail.com>
2022-05-24 20:55:47 -0700

    added error handling

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,39 +4,21 @@ function Contact() {
     return (
         <div id="contact">
             <h1>Contact Me</h1>
-            <form
-                name="contact"
-                method="post"
-                data-netlify="true"
-                onSubmit="submit"
-                data-netlify-honeypot="bot-field"
-            >
-                <input type="hidden" name="form-name" value="contact v2" />
-
-                <div hidden>
-                    <input name="bot-field" />
-                </div>
-
-                <div>
-                    <label>Name<br />
-                        <input type="text" name="full-name" />
-                    </label>
-                </div>
-
-                <div>
-                    <label htmlFor="email" >Email</label><br />
-                    <input id="email" type="email" name="email" />
-                </div>
-
-                <div>
-                    <label>Any Comments?<br />
-                        <textarea name="comments"></textarea>
-                    </label>
-                </div>
-
-                <button type="submit">Submit</button>
-
-            </form>
+            <form name="contact" method="post">
+            <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
         </div>
     )
 }
