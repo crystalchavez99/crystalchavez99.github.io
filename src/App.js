7f389014ef70b7dfb76cca7d4826257c32d62312
Crystal <crystal120599@gmail.com>
2022-05-24 21:14:11 -0700

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import NavBar from './Components/NavBar';
 import Projects from './Components/Projects';
 import Skills from './Components/Skills';
 function App() {
   return (
     <div>
+      <NavBar/>
       <About/>
       <Projects/>
       <Skills/>
---
+++ src/Components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavBar.css';
+function NavBar() {
+    return (
+        <div id="navbar">
+            <nav>
+                <a href="#about">
+                    Crystal Chavez
+                </a>
+                <a>Projects</a>
+                <a>Skills</a>
+                <a>Contact</a>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar;
