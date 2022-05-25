58d0b4f3e3b19da187154302d4550e3957856504
Crystal <crystal120599@gmail.com>
2022-05-24 20:36:13 -0700

    issue with form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,7 +4,7 @@ function Contact() {
     return (
         <div id="contact">
             <h1>Contact Me</h1>
-            <form name="contact" method="POST" netlify data-netlify-recaptcha="true" data-netlify="true">
+            <form name="contact" method="POST" data-netlify="true">
                 <p>
                     <label>Your Name: <input type="text" name="name" /></label>
                 </p>
