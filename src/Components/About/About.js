4851c8beb28c0b108e766eec11df7754d81bf12f
Crystal <crystal120599@gmail.com>
2022-05-27 13:58:37 -0700

    setting up projects style

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -11,27 +11,29 @@ function About() {
                 <div>
                     <img alt="profile-pic" />
                 </div>
-                <p>Hey there welcome to my portfolio!</p>
-                <p>If you read above this text then you know my name! You can call me either Crystal or Chavez,
-                    your preference but I'll respond to either one!
-                </p>
-                <p>I got introduced into the world of STEM from middle school where I took a summer class on video game development
-                    as I am a huge fun of video games. In that moment I knew programming was my calling, and was inspired to pursue it.
-                    From there I began to take computer science classes in high school, primarly AP Computer Science: Intro to Computer Science Principles and
-                    Intro to Java. After engaging and understanding the concepts of computer science fundamentals, it was an eye opener for me that technology can
-                    impact our world and set my career on software engineering.
-                </p>
-                <p>
-                    From there I went to community college where I obtained my Associate in Science for Transfer degree in Computer Science, but did
-                    not finish my Bachelor's due to COVID and the lack of resources from the university I transfered to so I had to postpone my
-                    education. This lead me to enroll to <a>App Academy</a> in their 24 week immersive software engineer online program. I have built
-                    four full stack applications with the technologies I learned such as React, Redux, Javascript, Flask, and many other technologies.
-                </p>
-                <p>I'm now at the end of my coding bootcamp quest and am seeking the first step to my software engineering career! I'm excited to begin
-                    my career as a software engineer and use my skills that this career demands. 
-                </p>
+                <div id="about-me">
+                    <p>Hey there welcome to my portfolio!</p>
+                    <p>If you read above this text then you know my name! You can call me either Crystal or Chavez,
+                        your preference but I'll respond to either one!
+                    </p>
+                    <p>I got introduced into the world of STEM from middle school where I took a summer class on video game development
+                        as I am a huge fun of video games. In that moment I knew programming was my calling, and was inspired to pursue it.
+                        From there I began to take computer science classes in high school, primarly AP Computer Science: Intro to Computer Science Principles and
+                        Intro to Java. After engaging and understanding the concepts of computer science fundamentals, it was an eye opener for me that technology can
+                        impact our world and set my career on software engineering.
+                    </p>
+                    <p>
+                        From there I went to community college where I obtained my Associate in Science for Transfer degree in Computer Science, but did
+                        not finish my Bachelor's due to COVID and the lack of resources from the university I transfered to so I had to postpone my
+                        education. This lead me to enroll to <a>App Academy</a> in their 24 week immersive software engineer online program. I have built
+                        four full stack applications with the technologies I learned such as React, Redux, Javascript, Flask, and many other technologies.
+                    </p>
+                    <p>I'm now at the end of my coding bootcamp quest and am seeking the first step to my software engineering career! I'm excited to begin
+                        my career as a software engineer and use my skills that this career demands.
+                    </p>
+                </div>
             </div>
-            <div id="info">
+            {/* <div id="info">
                 <div>
                     <a href="#contact">Work With Me</a>
                 </div>
@@ -41,7 +43,7 @@ function About() {
                 <div>
                     <a href="#skills">Skills and Technologies</a>
                 </div>
-            </div>
+            </div> */}
         </div>
     )
 }
