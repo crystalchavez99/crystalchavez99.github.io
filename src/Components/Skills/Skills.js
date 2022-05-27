598ac623fece1e6339ef5f770e66462136a2c071
Crystal <crystal120599@gmail.com>
2022-05-27 16:47:30 -0700

    looks good for now!

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -5,8 +5,27 @@ function Skills(){
     return(
         <div id="skills">
             <h1>Skills and Technologies</h1>
-            <p>React, Redux, JavaScript, SQL, HTML, CSS, PostgreSQL, Python, Express.js ,Node.js, SQLAlchemy ,
-Docker,Heroku, Flask, Sequelize, Git, AWS, Alembic, Github</p>
+<i class="devicon-react-original-wordmark"></i>
+<i class="devicon-redux-original"></i>
+<i class="devicon-javascript-plain"></i>
+<i class="devicon-postgresql-plain-wordmark"></i>
+<i class="devicon-html5-plain-wordmark"></i>
+<i class="devicon-css3-plain-wordmark"></i>
+<i class="devicon-python-plain-wordmark"></i>
+<i class="devicon-express-original-wordmark"></i>
+<i class="devicon-nodejs-plain-wordmark"></i>
+<i class="devicon-sqlalchemy-original-wordmark"></i>
+<i class="devicon-docker-plain-wordmark"></i>
+<i class="devicon-heroku-plain-wordmark"></i>
+<i class="devicon-flask-original-wordmark"></i>
+<i class="devicon-sequelize-plain-wordmark"></i>
+<i class="devicon-git-plain-wordmark"></i>
+<i class="devicon-amazonwebservices-plain-wordmark"></i>
+<i class="devicon-github-original-wordmark"></i>
+<i class="devicon-visualstudio-plain-wordmark"></i>
+<i class="devicon-ubuntu-plain-wordmark"></i>
+<i class="devicon-ubuntu-plain-wordmark"></i>
+<i class="devicon-java-plain-wordmark"></i>
             {/* <BadgeCheckIcon/>
             <ChipIcon/> */}
         </div>
