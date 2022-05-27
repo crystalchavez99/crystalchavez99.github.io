110302c0dfc431b942cd27522be4c8d6c9c265aa
Crystal <crystal120599@gmail.com>
2022-05-27 15:14:40 -0700

    got grid out!

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -10,42 +10,47 @@ function Projects() {
             {/* <CodeIcon/> */}
             <div id='project-list'>
                 <Box >
-                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                        <Grid xs={6} >
-                        <div className='project'>
-                            <h3>Gamingcloud</h3>
-                            <p className='project-description'>GamingCloud is a SoundCloud clone that focuses on both the listeners and the artists to embrace a gaming music
-                                platform</p>
-                            <p>Technologies: React, Redux, Express.js, Sequelize, PostgreSQL, Javascript, React Audio Player</p>
-                        </div>
+                    <Grid container rowSpacing={1} columnSpacing={{ xs: 1 }}>
+                        <Grid xs={5} >
+                            <div className='project'>
+                                <h3>Gamingcloud</h3>
+                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653501119/CLOUD_sskb9u.png'/>
+                                <p className='project-description'>GamingCloud is a SoundCloud clone that focuses on both the listeners and the artists to embrace a gaming music
+                                    platform</p>
+                                <p>Technologies: React, Redux, Express.js, Sequelize, PostgreSQL, Javascript, React Audio Player</p>
+                            </div>
                         </Grid>
 
-                        <Grid xs={6}>
+                        <Grid xs={5}>
                             <div className='project'>
                                 <h3>Boss-Shots</h3>
+                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653500499/BOSS_k1uqas.png'/>
                                 <p className='project-description'>Boss-Shots is a Flicker clone for sharing images of your epic boss battles from all your favorite video games.</p>
                                 <p>Technologies: React, Redux, JavaScript, PostgreSQL, Flask, SQLAlchemy, Alembic, AWS S3, Python</p>
                             </div>
                         </Grid>
-                        <Grid xs={6}>
+                        <Grid xs={5}>
                             <div className='project'>
                                 <h3>Pokezon</h3>
+                                <img src="https://res.cloudinary.com/dreambssd/image/upload/v1653506909/POKE_iphzvu.png"/>
                                 <p className='project-description'>Pokezon is a digital store application that sells supplies necessary for Pokémon training and lifestyle.</p>
                                 <p>Technologies: React, Redux, JavaScript, PostgreSQL, Flask, SQLAlchemy, Alembic, AWS S3, Python</p>
                             </div>
                         </Grid>
-                        <Grid xs={6}>
+                        <Grid xs={5}>
                             <div className='project'>
                                 <h3>Meme Overflow</h3>
-                                <p className='project-description'>Pokezon is a digital store application that sells supplies necessary for Pokémon training and lifestyle.</p>
-                                <p>Technologies: React, Redux, JavaScript, PostgreSQL, Flask, SQLAlchemy, Alembic, AWS S3, Python</p>
+                                <img src="https://res.cloudinary.com/dreambssd/image/upload/v1653689198/MEME_hfwysy.png"/>
+                                <p className='project-description'>Meme Overflow is a platform for users to ask questions, answer with memes, and comment on memes!</p>
+                                <p>Technologies: Pug.js, Express.js, JavaScript, PostgreSQL, Node.js, Sequelize, HTML,CSS, Heroku, Bcrypt</p>
                             </div>
                         </Grid>
-                        <Grid xs={6}>
+                        <Grid xs={5}>
                             <div className='project'>
                                 <h3>LengthConverter.net</h3>
-                                <p className='project-description'>Pokezon is a digital store application that sells supplies necessary for Pokémon training and lifestyle.</p>
-                                <p>Technologies: React, Redux, JavaScript, PostgreSQL, Flask, SQLAlchemy, Alembic, AWS S3, Python</p>
+                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653689194/length_tmv33e.png'/>
+                                <p className='project-description'>A Metric Converter site that focuses on Length Conversion.</p>
+                                <p>Technologies: Javascript, HTML, CSS, Netlify</p>
                             </div>
                         </Grid>
                     </Grid>
