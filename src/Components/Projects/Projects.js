1dae49e631bd959cccf2b955ed1249b4d2b1ab7b
Crystal <crystal120599@gmail.com>
2022-05-27 17:57:22 -0700

    added error issues

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -14,7 +14,7 @@ function Projects() {
                         <Grid xs={5} >
                             <div className='project'>
                                 <h3>Gamingcloud</h3>
-                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653501119/CLOUD_sskb9u.png'/>
+                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653501119/CLOUD_sskb9u.png' alt="home-page"/>
                                 <p className='project-description'>GamingCloud is a SoundCloud clone that focuses on both the listeners and the artists to embrace a gaming music
                                     platform</p>
                                 <p>Technologies: React, Redux, Express.js, Sequelize, PostgreSQL, Javascript, React Audio Player</p>
@@ -24,7 +24,7 @@ function Projects() {
                         <Grid xs={5}>
                             <div className='project'>
                                 <h3>Boss-Shots</h3>
-                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653500499/BOSS_k1uqas.png'/>
+                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653500499/BOSS_k1uqas.png' alt="home-page"/>
                                 <p className='project-description'>Boss-Shots is a Flicker clone for sharing images of your epic boss battles from all your favorite video games.</p>
                                 <p>Technologies: React, Redux, JavaScript, PostgreSQL, Flask, SQLAlchemy, Alembic, AWS S3, Python</p>
                             </div>
@@ -32,7 +32,7 @@ function Projects() {
                         <Grid xs={5}>
                             <div className='project'>
                                 <h3>Pokezon</h3>
-                                <img src="https://res.cloudinary.com/dreambssd/image/upload/v1653506909/POKE_iphzvu.png"/>
+                                <img src="https://res.cloudinary.com/dreambssd/image/upload/v1653506909/POKE_iphzvu.png" alt="home-page"/>
                                 <p className='project-description'>Pokezon is a digital store application that sells supplies necessary for Pokémon training and lifestyle.</p>
                                 <p>Technologies: React, Redux, JavaScript, PostgreSQL, Flask, SQLAlchemy, Alembic, AWS S3, Python</p>
                             </div>
@@ -40,7 +40,7 @@ function Projects() {
                         <Grid xs={5}>
                             <div className='project'>
                                 <h3>Meme Overflow</h3>
-                                <img src="https://res.cloudinary.com/dreambssd/image/upload/v1653689198/MEME_hfwysy.png"/>
+                                <img src="https://res.cloudinary.com/dreambssd/image/upload/v1653689198/MEME_hfwysy.png" alt="home-page"/>
                                 <p className='project-description'>Meme Overflow is a platform for users to ask questions, answer with memes, and comment on memes!</p>
                                 <p>Technologies: Pug.js, Express.js, JavaScript, PostgreSQL, Node.js, Sequelize, HTML,CSS, Heroku, Bcrypt</p>
                             </div>
@@ -48,7 +48,7 @@ function Projects() {
                         <Grid xs={5}>
                             <div className='project'>
                                 <h3>LengthConverter.net</h3>
-                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653689194/length_tmv33e.png'/>
+                                <img src='https://res.cloudinary.com/dreambssd/image/upload/v1653689194/length_tmv33e.png' alt="home-page"/>
                                 <p className='project-description'>A Metric Converter site that focuses on Length Conversion.</p>
                                 <p>Technologies: Javascript, HTML, CSS, Netlify</p>
                             </div>
